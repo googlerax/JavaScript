7c6d91069da12fd6b72ecc467befd2cd40b118a8
googlerax <46288252+googlerax@users.noreply.github.com>
2022-03-14 19:05:51 -0300

    Add files via upload
    Ejercicio n°2 de JavaScript

---
+++ Ejercicio_2.js
@@ -0,0 +1,14 @@
+/*En este ejercicio tienes que crear un archivo JavaScript donde crees una variable la cual contenga una cadena de texto.
+Después crearás una función, dentro de ella crearas una variable que se llame igual que la primera que has creado y le asignaras una cadena de texto diferente a la primera y mostrarás la variable por consola.
+Por último, fuera de la función tendrás que realizar la llamada a la función y mostrarás por consola la primera variable que has creado.*/
+
+let texto ="hola,estoy aprendiendo JavaScript" /* variable global*/
+console.log(texto)
+
+function miFuncion(){
+  let texto = "Estoy dentro de una funcion" /*variable local*/
+  console.log(texto)
+}
+miFuncion()
+
+console.log(texto)
