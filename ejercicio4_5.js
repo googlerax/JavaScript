eb3c7840d6fc5d5f3d8ae1db5c3050ca009bf5c5
googlerax <46288252+googlerax@users.noreply.github.com>
2022-03-16 16:53:36 -0300

    Add files via upload
    Ejercicios de la sesión 4 de JavaScript.

---
+++ ejercicio4_5.js
@@ -0,0 +1,11 @@
+/*En este primer ejercicio tienes que crear una cadena de texto con una frase, por ejemplo: “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. Por consola tienes que mostrar la frase con las palabras cambiadas.*/
+
+
+let frase = 'El perro de mi vecina es muy molesto'
+frase.replace('vecina','vecino')
+
+/*Como segundo ejercicio, tienes que crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.*/
+
+
+let text = 'es un lenguaje de programación ligero, interpretado o compilado justo-a-tiempo (just-in-time)'
+text.toUpperCase()
