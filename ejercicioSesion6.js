70fca368f58e4d3088b05d8b0a33cdda733bbd7a
googlerax <46288252+googlerax@users.noreply.github.com>
2022-03-24 19:50:25 -0300

    Add files via upload

---
+++ ejercicioSesion6.js
@@ -0,0 +1,15 @@
+//En este ejercicio tienes que rellenar un array con los números del 10 al 20 y mostrarlo por consola.//
+
+
+const array=[]
+
+for(let  i = 10; i<=20; i++) {
+
+    array.push(i)   
+    
+
+  }
+
+console.log("El contenido del array es:" + array)
+console.log(typeof array)
+console.log(array.length)
