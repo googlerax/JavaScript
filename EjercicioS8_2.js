45a63b83f5efb7e6e5c87061b0bfeb09fbf8f972
googlerax <46288252+googlerax@users.noreply.github.com>
2022-03-29 20:07:10 -0300

    Add files via upload

---
+++ EjercicioS8_2.js
@@ -0,0 +1,28 @@
+//En este ejercicio tienes que crear un archivo javascript que contenga una función llamada eliminarUltimo() que reciba como parámetro un array y elimine el último elemento de este.
+//En el mismo archivo del ejercicio 2, modifica la función para que alerte al usuario cuando el parámetro no sea un array o este esté vacío.
+//Si el parámetro es un array con más de un elemento, la función debe realizar lo descrito en el ejercicio anterior
+
+function eliminarUltimo(array){
+
+   return array.pop()
+    
+
+}
+
+let miArray = [1,4,4,7,"hola"]
+
+
+eliminarUltimo(miArray)
+
+console.log(miArray)
+
+
+
+//ejercicio incompleto
+
+
+
+
+
+
+
---
+++ EjerciciosS8_1.js
@@ -0,0 +1,40 @@
+
+
+function suma(a,b){
+    resultado1 = a + b
+    return resultado1
+}
+
+console.log(suma(7,6))
+
+
+
+function resta(a,b){
+    resultado2 = a - b
+    return resultado2
+}
+
+console.log(resta(7,6))
+
+
+function multi(a,b){
+    resultado3 = a * b
+    return resultado3
+}
+
+console.log(multi(7,6))
+
+
+
+function dividir(a,b){
+
+    if(b===0){
+    console.log("no se puede dividir por 0")
+    }
+    resultado4= a / b
+    return resultado4
+}
+
+console.log(dividir(7,0))
+
+
