15c48663d432ec24ce239ee9fdb0ed8f9ca7814e
googlerax <46288252+googlerax@users.noreply.github.com>
2022-05-10 20:07:46 -0300

    ejercicio n° 3 resubido.

---
+++ ejer_3/factorial-break.js
@@ -0,0 +1,17 @@
+
+
+let factor = 1;
+let numero = 10;
+
+
+while (true) {
+    
+    factor *= numero;
+    numero--;
+
+    if (numero === 1) break;
+        
+    
+}
+
+console.log(factor);
---
+++ ejer_3/factorial-while.js
@@ -0,0 +1,12 @@
+
+let factory = 1;
+let nume = 10;
+
+
+while (nume > 1) {
+    
+    factory *= nume
+    nume--
+}
+
+console.log(factory);
